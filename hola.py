cef021f49a3831b73645137816964eb06b88ff88
ferstrak <fernandobrana.r@gmail.com>
2025-04-16 16:40:44 -0400

    primer commit

---
+++ hola.py
@@ -0,0 +1,71 @@
+# nombre="diego"
+# edad=25
+# print ("hola",nombre, "su edad es",edad)
+
+# function (argumento de la funccion)
+
+
+
+# ///////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+# solicitud de datos
+# print ("ingrese su nombre")
+# nombre=input()
+# print ("ingrese su edad")
+# edad=input()
+# # ejemplo de concatenación
+# print ("hola",nombre, "su edad es",edad)
+# print ("ingrese 2 numeros ")
+# 7
+# n1=int(input())
+# n2=int(input())
+# print ("el resultado es", n1 + n2)
+
+# /////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+# print ("ingrese 3 numeros para promedio ")
+# n1=int(input())
+# n2=int(input())
+# n3=int(input())
+# prom=(n1+n2+n3)/3
+# print ("el promedio es",prom)
+
+# if prom>=40:
+#     print("el alumno aprobo")
+# else:
+#     print("el alumno reprobo")    
+# /////////////////////////////////////////////////////////////////////////////////////////////////
+# print ("ingrese su nombre")
+# nombre=input()
+# edad =int(input("ingrese su edad")) 
+
+
+# if edad>=18:
+#    print ("mayor de edad")
+# else:
+#    print ("menor de edad")
+
+
+# ////////////////////////////////////////////
+# edad =int(input("ingrese su edad")) 
+# if edad<12:
+#    print ("usted es un niño")
+# elif edad >=12 and edad<18:
+#    print ("usted es un adolescente")
+# elif edad >=18 and edad<65:
+#    print ("usted es un adulto")  
+# else:
+#    print ("usted es un viejo")
+
+# //////////////////////////////////////////////////////////////////////////////////
+
+print ("ingrese 3 numeros")
+n1=int(input())
+n2=int(input())
+n3=int(input())
+if n1>n2 and n1>n3:
+ print (n1,"es mayor")
+elif n2>n1 and n2>n3:
+ print (n2,"es mayor") 
+else:
+ print (n3,"es mayor") 
