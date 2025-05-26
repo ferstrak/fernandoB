3b08e134a082f96062ed030213a48156cd900b94
ferstrak <fernandobrana.r@gmail.com>
2025-05-26 11:16:02 -0400

    nuevos ejercicios

--- hola.py
+++ hola.py
@@ -1059,16 +1059,237 @@
 #        print("solo se aceptan numeros enteros dentro del rango")
 # for i in range (serv):
 
+# //////////////////////////////////////////////////////////////////////////////////////////////
 
-# profe:
+# actividad 2.5.3
+
+# El programa debe tener un menú de opciones de donde se pueda realizar el pago
+# del cupo de la tarjeta de crédito, como también simular nuevas compras, y estas
+# una vez sumadas se resten al cupo disponible.
+# Las opciones disponibles deben estar construidas de la siguiente forma:
+
+# 1. Pago de Tarjeta de Crédito:
+
+# a. El usuario comienza con una deuda de $100.000
+
+# b. El usuario puede ingresar un monto para realizar un pago en la
+# tarjeta de crédito.
+
+# c. Se debe verificar que el monto ingresado sea mayor o igual a cero.
+
+# d. Se debe verificar que el monto a pagar no exceda el saldo actual de
+# la tarjeta.
+
+# e. Al pagar el sistema debe descontar de la deuda total
+
+# f. Si las verificaciones son exitosas, se realiza el pago y se actualiza el
+# saldo de la tarjeta.
 
 
 
+# 2. Simulación de Compras:
+
+# a. El usuario puede simular realizar un número ilimitado de compras.
+
+# b. Para cada compra, se solicita al usuario ingresar el monto de la
+# compra. El programa suma los montos de cada compra.
+
+# c. Se verifica que el monto de la compra sea mayor o igual a cero.
+
+# d. Se realiza la compra y se actualiza el saldo de la tarjeta para cada
+# iteración del bucle for.
+
+
+
+
+# 3. Salir:
+# a. Al seleccionar esta opción, el programa debe cerrarse o finalizar considerar:
+
+
+
+# 1. Manejo de Errores:
+
+# a. Se utilizan bloques try y except para manejar posibles errores al
+
+# ingresar datos, validar valores no numéricos y errores inesperados.
+
+# 2.b. Se debe programar mensajes de error específicos para guiar al
+# usuario sobre posibles problemas
+
+
+# yo
+# montotaldeuda=100.000
+# import time
+# print ("bienvenido al programa de pago de tarjetas. Ingrese su numero de rut")
+# rut=int(input())
+# print ("tras revisar su cuenta debe un monto de 100.000")
+# time.sleep(1)
+# print ("cuanto desea pagar?")
+# montopagar=float(input())
+# if montopagar<0:
+#     print("ingrese un numero valido")
+# if montopagar>=montotaldeuda:
+#     print ("has superado el saldo actual de la tarjeta.")
+
+# elif montopagar<montotaldeuda:
+#     print("el saldo actual de la tarjeta es", montotaldeuda-montopagar)
+
+
+
+
+    # ///////////////////////////////////////////////////////////////////////////////////////////++
+
+
+
+  
+
+
+
+# profe
+
+
+# deuda=100000
+
+# while True:
+#     op=int(input('''
+# seleccione una opción
+#      1.- pago
+#      2.- compra            
+#      3.- salir            
+# '''))
     
-           
+#     match op:
+#         case 1:
+#             print(f"la deuda actual es {deuda}")
+#             while True:
+#                 try:
+#                     pago=int(input("ingrese el monto a pagar"))
+#                     break
+#                 except Exception:
+#                     print("solo se a admiten numeros enteros")
+#             if pago>0 and pago<=deuda:
+#                 deuda=deuda-pago
+
+#             else:
+#                 print(f"el pago debe ser mayor a cero y no exceder {deuda}")        
+
+            
+
+#         case 2:
+#             print("comprando")
+#             monto=int(input("ingrese el monto de la compra: "))
+#             deuda+=monto
+#             print(f"la deuda actual es {deuda}")
+
+
+#         case 3:
+#             print ("saliendo...")
+#             break
+
+
+#         case _:
+#             print("opcion invalida")
     
+# ////////////////////////////////////////////////////////////////////////////////////////////////////
+# Debe crear un menú de inicio de sesión, en el cual se debe mostrar los siguientes campos:
+# 1) iniciar sesión
+# 2) registrar usuario
+# 3) salir
+# Para lo cual usted deberá haber creado 3 variables de usuario y 3 variables de contraseña,
+# ambas con valor inicial vacío, ejemplo:
+# • usuario1= None
+# • usuario2=None
+# • usuario3=None
+# • contrasena1= None
+# • contrasena2=None
+# • contrasena3= None
+# Si se selecciona la opción 1 y no existen registros de usuarios, el sistema deberá indicar que
+# es necesario registrar un usuario antes, y volverá al menú principal, en el caso de que
+# ingrese el usuario y contraseña correctamente, entonces el sistema mostrará el siguiente
+# menú:
+# 1) Realizar llamada
+# 2) Enviar correo electrónico
+# 3) Cerrar sesión
+# 2
+# Donde la opción 1 debe solicitar un número de celular, éste deberá comenzar con 9 y su
+# tamaño es de 9 dígitos (ejemplo: 985447561).
+
+# La opción 2, solicita un correo electrónico, el cual debe tener por lo menos un carácter de
+#  (validar usando for y while) y lo guardará en una variable llamada “correo”.
+
+# También solicitará el mensaje a enviar y lo guardará en una variable llamada “mensaje”
+
+# Finalmente cerrar sesión, volverá al menú principal.
+
+# El sistema no acepta que se ingresen opciones distintas a 1, 2 y 3 en ambos menús, si ocurre
+# esto, entonces el sistema emite un error y vuelve a solicitar la opción.
+
+# Recuerde utilizar try Exception en caso de ser necesario
 
 
+usuario1= None
+usuario2=None
+usuario3=None
+contrasena1= None
+contrasena2=None
+contrasena3= None
+usertrycount=0
+userpasswdtrycount=0
+while True:
+    op=int(input ('''
+                  selecciona una opcion
+                  1.- iniciar sesión
+                  2.- registrar usuario 
+                  3.- salir
+                  '''))
+    match op:
+        case 1:
+            print ("ingrese usuario")
+            usertry=input (())
+            if (usertry!=usuario1)(usertry!=usuario2) and (usertry!=usuario3):
+               print("usuario no encontrado")
+               userpasswdtrycount+=1
+            else:
+                print(" usuario encontrado")
+
+            while
+            print("ingrese contraseña")
+            userpasswdtry=input (())
+            if (userpasswdtry!=contrasena1)(userpasswdtry!=contrasena2) and (userpasswdtry!=contrasena3):
+               print("contraseña incorrecta")
+               userpasswdtrycount+=1
+            else:
+                print("bienvenido")
+        case 2:
+        case 3:
+            break
+        case _:
+            print("opción invalida")
+    # ///////////////////////////////////////////////////////////////////////////////////////////
+
+
+usuario1= None
+usuario2=None
+usuario3=None
+contrasena1= None
+contrasena2=None
+contrasena3= None
+usertrycount=0
+userpasswdtrycount=0
+while True:
+    op=int(input ('''
+                  selecciona una opcion
+                  1.- iniciar sesión
+                  2.- registrar usuario 
+                  3.- salir
+                  '''))
+    match op:
+        case 1:
+        case 1:
+        case 1:
+        case 1:
+
+usar listas
 
 
 
